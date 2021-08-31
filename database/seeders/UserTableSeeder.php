b652c39cb86e5bad6accda2aafaad636ce2c5566
Maxuel Rodrigues <maxuel.rodrigues@gmail.com>
2021-08-31 09:47:18 -0300

    seed user

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'name' => 'Administrador',
+            'email' => 'user@vivereplus.com',
+            'password' => 'vivereplus@123'
+        ]);
+    }
+}
