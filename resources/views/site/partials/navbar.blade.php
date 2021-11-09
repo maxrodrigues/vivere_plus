e3138e83ea7ffdf32cc7fbc1d5dc7475c5af12a5
Max Rodrigues <maxuel.rodrigues@gmail.com>
2021-11-09 17:10:12 -0300

    comentario temporario

--- resources/views/site/partials/navbar.blade.php
+++ resources/views/site/partials/navbar.blade.php
@@ -1,4 +1,5 @@
-<header id="header" class="header-transparent header-transparent header-transparent-dark" data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': true, 'stickyChangeLogo': true, 'stickyStartAt': 30, 'stickyHeaderContainerHeight': 70}">
+<header id="header" class="header-transparent header-transparent header-transparent-dark"
+    data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': true, 'stickyChangeLogo': true, 'stickyStartAt': 30, 'stickyHeaderContainerHeight': 70}">
     <div class="header-body border-top-0 bg-dark box-shadow-none">
         <div class="header-top header-top-borders header-top-light-2-borders">
             <div class="container h-100">
@@ -48,56 +49,66 @@
                     <div class="header-row">
                         <div class="header-logo">
                             <a href="index.html">
-                                <img alt="Vivere Plus" width="140" height="42" data-sticky-width="140" data-sticky-height="42" src="{{ asset('img/logo-branca-pequena.png') }}">
+                                <img alt="Vivere Plus" width="140" height="42" data-sticky-width="140"
+                                    data-sticky-height="42" src="{{ asset('img/logo-branca-pequena.png') }}">
                             </a>
                         </div>
                     </div>
                 </div>
                 <div class="header-column justify-content-end">
                     <div class="header-row">
-                        <div class="header-nav header-nav-links header-nav-dropdowns-dark header-nav-light-text order-2 order-lg-1">
-                            <div class="header-nav-main header-nav-main-mobile-dark header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-2 header-nav-main-sub-effect-1">
+                        <div
+                            class="header-nav header-nav-links header-nav-dropdowns-dark header-nav-light-text order-2 order-lg-1">
+                            <div
+                                class="header-nav-main header-nav-main-mobile-dark header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-2 header-nav-main-sub-effect-1">
                                 <nav class="collapse">
                                     <ul class="nav nav-pills" id="mainNav">
                                         <li class="dropdown">
-                                            <a class="dropdown-item dropdown-toggle @if($active == 'home') active @endif" href="{{ route('home') }}">
+                                            <a class="dropdown-item dropdown-toggle @if ($active == 'home') active @endif"
+                                                href="{{ route('home') }}">
                                                 Home
                                             </a>
                                         </li>
                                         <li class="dropdown dropdown-mega">
-                                            <a class="dropdown-item dropdown-toggle @if($active == 'about') active @endif" href="{{ route('about') }}">
+                                            <a class="dropdown-item dropdown-toggle @if ($active == 'about') active @endif"
+                                                href="{{ route('about') }}">
                                                 A Vivere
                                             </a>
                                         </li>
-                                        <li class="dropdown">
-                                            <a class="dropdown-item dropdown-toggle @if($active == 'buildings') active @endif" href="{{ route('buildings') }}">
+                                        {{-- <li class="dropdown">
+                                            <a class="dropdown-item dropdown-toggle @if ($active == 'buildings') active @endif" href="{{ route('buildings') }}">
                                                 Empreendimentos
                                             </a>
                                         </li>
                                         <li class="dropdown">
-                                            <a class="dropdown-item dropdown-toggle @if($active == 'blog') active @endif" href="{{ route('blog') }}">
+                                            <a class="dropdown-item dropdown-toggle @if ($active == 'blog') active @endif" href="{{ route('blog') }}">
                                                 Blog
                                             </a>
                                         </li>
                                         <li class="dropdown">
-                                            <a class="dropdown-item dropdown-toggle @if($active == 'contact-us') active @endif" href="{{ route('contact-us') }}">
+                                            <a class="dropdown-item dropdown-toggle @if ($active == 'contact-us') active @endif" href="{{ route('contact-us') }}">
                                                 Contato
                                             </a>
-                                        </li>
+                                        </li> --}}
                                     </ul>
                                 </nav>
                             </div>
-                            <button class="btn header-btn-collapse-nav" data-toggle="collapse" data-target=".header-nav-main nav">
+                            <button class="btn header-btn-collapse-nav" data-toggle="collapse"
+                                data-target=".header-nav-main nav">
                                 <i class="fas fa-bars"></i>
                             </button>
                         </div>
-                        <div class="header-nav-features header-nav-features-light header-nav-features-no-border header-nav-features-lg-show-border order-1 order-lg-2">
+                        <div
+                            class="header-nav-features header-nav-features-light header-nav-features-no-border header-nav-features-lg-show-border order-1 order-lg-2">
                             <div class="header-nav-feature header-nav-features-search d-inline-flex">
-                                <a href="#" class="header-nav-features-toggle" data-focus="headerSearch"><i class="fas fa-search header-nav-top-icon"></i></a>
-                                <div class="header-nav-features-dropdown header-nav-features-dropdown-mobile-fixed" id="headerTopSearchDropdown">
+                                <a href="#" class="header-nav-features-toggle" data-focus="headerSearch"><i
+                                        class="fas fa-search header-nav-top-icon"></i></a>
+                                <div class="header-nav-features-dropdown header-nav-features-dropdown-mobile-fixed"
+                                    id="headerTopSearchDropdown">
                                     <form role="search" action="page-search-results.html" method="get">
                                         <div class="simple-search input-group">
-                                            <input class="form-control text-1" id="headerSearch" name="q" type="search" value="" placeholder="Search...">
+                                            <input class="form-control text-1" id="headerSearch" name="q" type="search"
+                                                value="" placeholder="Search...">
                                             <span class="input-group-append">
                                                 <button class="btn" type="submit">
                                                     <i class="fa fa-search header-nav-top-icon text-color-dark"></i>
@@ -132,27 +143,27 @@
                                 <nav class="collapse">
                                     <ul class="nav nav-pills" id="mainNav">
                                         <li class="dropdown">
-                                            <a class="dropdown-item dropdown-toggle @if($active == 'home') active @endif" href="{{ route('home') }}">
+                                            <a class="dropdown-item dropdown-toggle @if ($active == 'home') active @endif" href="{{ route('home') }}">
                                                 Home
                                             </a>
                                         </li>
                                         <li class="dropdown dropdown-mega">
-                                            <a class="dropdown-item dropdown-toggle @if($active == 'about') active @endif" href="{{ route('about') }}">
+                                            <a class="dropdown-item dropdown-toggle @if ($active == 'about') active @endif" href="{{ route('about') }}">
                                                 A Vivere
                                             </a>
                                         </li>
                                         <li class="dropdown">
-                                            <a class="dropdown-item dropdown-toggle @if($active == 'buildings') active @endif" href="{{ route('buildings') }}">
+                                            <a class="dropdown-item dropdown-toggle @if ($active == 'buildings') active @endif" href="{{ route('buildings') }}">
                                                 Empreendimentos
                                             </a>
                                         </li>
                                         <li class="dropdown">
-                                            <a class="dropdown-item dropdown-toggle @if($active == 'blog') active @endif" href="{{ route('blog') }}">
+                                            <a class="dropdown-item dropdown-toggle @if ($active == 'blog') active @endif" href="{{ route('blog') }}">
                                                 Blog
                                             </a>
                                         </li>
                                         <li class="dropdown">
-                                            <a class="dropdown-item dropdown-toggle @if($active == 'contact-us') active @endif" href="{{ route('contact-us') }}">
+                                            <a class="dropdown-item dropdown-toggle @if ($active == 'contact-us') active @endif" href="{{ route('contact-us') }}">
                                                 Contato
                                             </a>
                                         </li>
