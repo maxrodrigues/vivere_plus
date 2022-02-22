d36d38530e5d74461277ae71bd5030a76d6cb9d4
Maxuel Rodrigues <maxuel.rodrigues@gmail.com>
2022-02-22 10:04:34 -0300

    completion of the golden sea building page

--- resources/views/site/pairs/galwan/entregues/mar-dourado.blade.php
+++ resources/views/site/pairs/galwan/entregues/mar-dourado.blade.php
@@ -25,7 +25,7 @@
 
     <section class="section section-height-3 m-0 border-0" style="background-color: #2c247b">
         <div class="container">
-            <div class="row align-items-center">
+            <div class="row">
                 <div class="col-lg-6">
                     <div class="pr-5">
                         <h2 class="font-weight-bold text-color-light negative-ls-3 line-height-2 text-9 mb-3">
@@ -96,10 +96,10 @@
                                 </ul>
                             </div>
                         </div>
-                        <button class="btn btn-light btn-rounded text-3 font-weight-bold mr-3" style="color: #2c247b">
+                        <a href="#galery" class="btn btn-light btn-rounded text-3 font-weight-bold mr-3" style="color: #2c247b">
                             <i class="fa fa-plus mr-1"></i>
                             Informações
-                        </button>
+                        </a>
                         <a href="https://api.whatsapp.com/send?phone=+5527997333555" target="_blank" class="btn btn-success btn-rounded text-3 font-weight-bold mr-3">
                             <i class="fab fa-whatsapp mr-1"></i>
                             Contate-nos
@@ -115,7 +115,7 @@
         </div>
     </section>
 
-    <section class="section section-height-3 m-0 border-0">
+    <section class="section section-height-3 m-0 border-0" id="galery">
         <div class="container">
             <div class="row">
                 <div class="col">
@@ -394,5 +394,124 @@
                     </div>
                 </div>
             </div>
+    </section>
+
+    <section class="section sectin-height-3 m-0 border-0" style="background-color: #2c247b">
+        <div class="container">
+            <div class="row">
+                <h4 class="font-weight-bold text-center text-color-light negative-ls-3 line-height-2 text-9 mb-3 w-100">
+                    Plantas
+                </h4>
+                <div class="mt-4 pt-2 lightbox" data-plugin-options="{'delegate': 'a', 'type': 'image', 'gallery': {'enabled': true}}">
+                    <div class="row portfolio-list sort-destination" data-sort-id="portfolio">
+
+                        <div class="col-12 col-sm-6 col-lg-3 isotope-item">
+                            <div class="portfolio-item">
+                                <a href="{{ asset('img/pairs/galwan/empreendimentos/mar-dourado/plantas/coluna-1-2-md.jpg') }}">
+                                    <span class="thumb-info thumb-info-lighten border-radius-0">
+                                        <span class="thumb-info-wrapper border-radius-0">
+                                            <img src="{{ asset('img/pairs/galwan/empreendimentos/mar-dourado/plantas/coluna-1-2-md.jpg') }}" class="img-fluid border-radius-0" alt="">
+                                        </span>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-sm-6 col-lg-3 isotope-item">
+                            <div class="portfolio-item">
+                                <a href="{{ asset('img/pairs/galwan/empreendimentos/mar-dourado/plantas/coluna-3-4-md.jpg') }}">
+                                    <span class="thumb-info thumb-info-lighten border-radius-0">
+                                        <span class="thumb-info-wrapper border-radius-0">
+                                            <img src="{{ asset('img/pairs/galwan/empreendimentos/mar-dourado/plantas/coluna-3-4-md.jpg') }}" class="img-fluid border-radius-0" alt="">
+                                        </span>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-sm-6 col-lg-3 isotope-item">
+                            <div class="portfolio-item">
+                                <a href="{{ asset('img/pairs/galwan/empreendimentos/mar-dourado/plantas/coluna-5-6-md.jpg') }}">
+                                    <span class="thumb-info thumb-info-lighten border-radius-0">
+                                        <span class="thumb-info-wrapper border-radius-0">
+                                            <img src="{{ asset('img/pairs/galwan/empreendimentos/mar-dourado/plantas/coluna-5-6-md.jpg') }}" class="img-fluid border-radius-0" alt="">
+                                        </span>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-sm-6 col-lg-3 isotope-item">
+                            <div class="portfolio-item">
+                                <a href="{{ asset('img/pairs/galwan/empreendimentos/mar-dourado/plantas/coluna-7-8-md.jpg') }}">
+                                    <span class="thumb-info thumb-info-lighten border-radius-0">
+                                        <span class="thumb-info-wrapper border-radius-0">
+                                            <img src="{{ asset('img/pairs/galwan/empreendimentos/mar-dourado/plantas/coluna-7-8-md.jpg') }}" class="img-fluid border-radius-0" alt="">
+                                        </span>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section section-height-3 m-0 border-0" style="background-color: #2c247b">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h2 class="font-weight-bold text-color-light negative-ls-3 line-height-2 text-9 mb-3">
+                        Interessado? Preencha o formulário.
+                    </h2>
+                    <div class="row">
+                        <div class="col-12">
+                            <form class="contact-form" action="{{ route('contact-us.sendMail') }}" method="POST">
+                                @csrf
+                                <div class="form-group">
+                                    <input type="text" name="name" id="name" class="form-control" placeholder="Nome" style="border-radius: 0; height: 50px; padding: 10px; font-size: 15px; color: #777;">
+                                </div>
+                                <div class="form-group">
+                                    <input type="tel" name="phone" id="phone" class="form-control" placeholder="Telefone" style="border-radius: 0; height: 50px; padding: 10px; font-size: 15px; color: #777;">
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" name="email" id="email" class="form-control" placeholder="E-mail" style="border-radius: 0; height: 50px; padding: 10px; font-size: 15px; color: #777;">
+                                </div>
+                                <div class="form-group">
+                                    <textarea name="message" id="message" cols="30" rows="10" placeholder="Mensagem" class="form-control" style="border-radius: 0; padding: 10px; font-size: 15px; color: #777;"></textarea>
+                                </div>
+                                <button class="btn btn-danger btn-rounded w-25">Enviar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <h2 class="font-weight-bold text-color-light negative-ls-3 line-height-2 text-9 mb-3 mt-0">
+                        Ou entre em contato diretamente com um consultor.
+                    </h2>
+                    <div class="row">
+                        <div class="col">
+                            <a href="#" class="btn btn-light ml-2" style="border-radius: 100%; width: 50px; height: 50px; padding: 13px 0;">
+                                <i class="fab fa-whatsapp text-6"></i>
+                            </a>
+                            {{-- <a href="#" class="btn btn-light ml-2" style="border-radius: 100%; width: 50px; height: 50px; padding: 13px 0;">
+                                <i class="fab fa-whatsapp mr-1"></i>
+                            </a> --}}
+                            <a href="#" class="btn btn-light ml-2" style="border-radius: 100%; width: 50px; height: 50px; padding: 13px 0;">
+                                <i class="fab fa-instagram text-6"></i>
+                            </a>
+                            <a href="#" class="btn btn-light ml-2" style="border-radius: 100%; width: 50px; height: 50px; padding: 13px 0;">
+                                <i class="fab fa-facebook text-6"></i>
+                            </a>
+                            <a href="#" class="btn btn-light ml-2" style="border-radius: 100%; width: 50px; height: 50px; padding: 13px 0;">
+                                <i class="fab fa-youtube text-6"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
